de7d5490aa9616cc747f05b791378f959bb3bb15
npatel5487 <49158107+npatel5487@users.noreply.github.com>
2024-11-09 12:26:16 -0500

    Added Tab functionality and headers

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,18 @@
+
+export function TodoCard(props) {
+    const { todo } = props
+    console.log(todo)
+    return (
+        <div className="card todo-item" >
+            <p>{todo.input}</p>
+            <div className="todo-buttons">
+                 <button disabled={todo.complete}>
+                    <h6>Done</h6>
+                 </button>
+                 <button>
+                    <h6>Delete</h6>
+                 </button>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,24 @@
+import {TodoCard} from "./TodoCard";
+
+export function TodoList(props){
+    const { todos } = props
+    
+    const tab = 'All'
+    const filterTodosList = tab === 'All' ?
+        todos :
+        tab === 'Completed' ?
+            todos.filter(val=> val.complete):
+            todos.filter(val=> !val.complete)
+
+    return(
+        <>
+            {filterTodosList.map((todo, todoIndex) => {
+                return(
+                    <TodoCard 
+                        key={todoIndex}  
+                        todo={todo}/>
+                )
+            })}
+        </>
+    )
+}
