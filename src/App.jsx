7a89e13b95ff7362883650674d111f2388b0d102
npatel5487 <49158107+npatel5487@users.noreply.github.com>
2024-11-13 20:22:33 -0500

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header"
 import { Tabs } from "./components/Tabs"
 import { TodoInput } from "./components/TodoInput"
-import { TodoList } from "./components/ToDoList"
+import { TodoList } from "./components/TodoList"
 import {useState, useEffect} from 'react'
 
 function App() {
